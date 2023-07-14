1bdc0926257ea5feb2342f2b806c546bae37831f
liweiran <lwr@bupt.edu.cn>
2023-07-15 01:19:47 +0800

    [feat]新增页面跳转功能，初始页面

--- app/src/main/java/com/example/wachat/MainActivity.java
+++ app/src/main/java/com/example/wachat/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.wachat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button regist_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        regist_btn = (Button) findViewById(R.id.btn_regist);
+        regist_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, RegistActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
